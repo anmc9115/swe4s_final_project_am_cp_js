aaf512783cdd5d950dddc4d87813fe59134d347f
anmc9115 <67393986+anmc9115@users.noreply.github.com>
2020-10-14 16:34:48 -0600

    Delete fpho_driver.py

--- fpho_driver.py
+++
@@ -1,7 +0,0 @@
-import fpho_setup as fps
-
-fTime, f1Red, f1Green, f2Red, f2Green = fps.import_fpho_data('2FiberTest.csv')
-
-# Argparse for fpho_setup will go here
-# This file is analagous to print_cases
-# fpho_setup is analagous to my_utils
