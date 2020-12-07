ec234b15ca249b30d62a0a3b7600c29412bcfa19
josh-seabaugh <jose3030@colorado.edu>
2020-12-07 06:09:00 +0000

    more unit testing

--- Python/test_fpho_setup.py
+++ Python/test_fpho_setup.py
@@ -5,6 +5,8 @@ import unittest
 import random
 import sys
 import pandas as pd
+import os.path
+from os import path
 
 
 class TestFphoSetup(unittest.TestCase):
@@ -45,12 +47,49 @@ class TestFphoSetup(unittest.TestCase):
         self.assertEqual(0.7357588823428847, fit[0])
         self.assertEqual(9.079985952496971e-05, fit[3])
 
-#    def test_raw_signal_trace_errors(self):
-#        with self.assertRaises(SystemExit) as cm:
-#            fpho_setup.raw_signal_trace(fpho_dataframe='bad_df.csv',
-#                                        output_filename='testing_unit.png',
-#                                        data_row_index=0)
-#        self.assertEqual(cm.exception.code, 1)
+    def test_raw_signal_trace(self):
+        df_test = fpho_setup.import_fpho_data(animal_ID='vole1',
+                                         exp_date='2020-09-01',
+                                         exp_desc="testing",
+                                         input_filename='TestData/1FiberTesting.csv',
+                                         output_filename='testing_unit.csv')
+        fpho_setup.raw_signal_trace(fpho_dataframe=df_test,
+                                        output_filename='testing_unit.png',
+                                        data_row_index=0)
+        self.assertTrue(path.exists('testing_unit_f1RedRed_rawsig.png'))
+
+    def test_raw_signal_trace_errors(self):
+        df_test = fpho_setup.import_fpho_data(animal_ID='vole1',
+                                         exp_date='2020-09-01',
+                                         exp_desc="testing",
+                                         input_filename='TestData/1FiberTesting.csv',
+                                         output_filename='testing_unit.csv')
+        with self.assertRaises(SystemExit) as cm:
+            fpho_setup.raw_signal_trace(fpho_dataframe=df_test,
+                                        output_filename='testing_unit.png',
+                                        data_row_index=0)
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_plot_1fiber_norm_iso(self):
+        df_test = fpho_setup.import_fpho_data(animal_ID='vole1',
+                                         exp_date='2020-09-01',
+                                         exp_desc="testing",
+                                         input_filename='TestData/1FiberTesting.csv',
+                                         output_filename='testing_unit.csv')
+        norm = fpho_setup.plot_1fiber_norm_iso(fpho_dataframe=df_test)
+        self.assertTrue(path.exists('f1GreenNormIso.png'))
+        self.assertTrue(path.exists('f1RedNormIso.png'))
+
+    def test_plot_1fiber_norm_fitted(self):
+        df_test = fpho_setup.import_fpho_data(animal_ID='vole1',
+                                         exp_date='2020-09-01',
+                                         exp_desc="testing",
+                                         input_filename='SampleData/1fiberSignal.csv',
+                                         output_filename='testing_unit.csv')
+        fit = fpho_setup.plot_1fiber_norm_fitted(fpho_dataframe=df_test)
+        self.assertTrue(path.exists('f1GreenNormExp.png'))
+        self.assertTrue(path.exists('f1RedNormExp.png'))
+
 
 if __name__ == '__main__':
     unittest.main()
